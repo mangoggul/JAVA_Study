f2d8b2fcfb7b95da75e661f152fcdda78e2d1270
mango <darkdon66@naver.com>
2024-08-13 15:10:55 +0900

    Feat : Sample

---
+++ Sample.java
@@ -0,0 +1,14 @@
+public class Sample {
+    
+    int base = 100;
+    int height = 185;
+
+    boolean isTall = height > base;
+    public static void main(String[] args){
+        Sample sample = new Sample();
+        System.out.println(7%2);
+        if (sample.isTall) {
+            System.out.println("키가커용");
+        }
+    }
+}
--- study1.java
+++ study1.java
@@ -1,5 +1,28 @@
-public class study1 {
+public class study1 { //클래스 명은 파일이랑 이름 같아야
+
+
+    int a;
+    String b = "Hello World";
+    //변수 이름은 3가지 규칙 : 변수명은 숫자로 시작 x , _와 $외의 특수문자 x , 자바 키워드는 변수명
+    //클래스 멤버 변수는 클래스의 메서드 내에서 초기화 or 변수선언시 초기화
+
+    public study1() { //이거를 메서드라고 함
+        a = 1;
+        b = "Hello Java";
+    }
     public static void main(String[] args){
-        System.out.print("hello world");
+        study1 instance = new study1(); //메서드 안을 명령문이라고 함
+        System.out.println(instance.b);
+        System.out.println("hello world");
+    }
+
+    public static void main2(String[] args) {
+        System.out.println(7%2);
     }
 }
+
+
+//클라스 명은 대문자로 시작하고 명사이다. PascalCase로 짓는다. 암묵적인 룰
+class ChocoCookie{}
+//메서드 명은 동사이고 소문자로 시작한다. 이 역시 pascalCase를 이용.
+
