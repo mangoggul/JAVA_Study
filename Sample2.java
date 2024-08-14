f14465b87a1935969a3e49da1aa0ca1bdee82920
mango <darkdon66@naver.com>
2024-08-14 10:48:33 +0900

    Feat : List

--- Sample2.java
+++ Sample2.java
@@ -1,4 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ArrayList;
 public class Sample2 {
+
+
+    int[] odds = {1,3,5,7,9};
+    String[] weeks = {"월", "화","수","목","금","토","일"};
+
 
     public void stringMethod(){
         String result = "";
@@ -6,20 +14,23 @@ public class Sample2 {
         result += " ";
         result += "jump to java";
         System.out.println(result);
+        Sample2 sample = new Sample2();
+        System.out.println(Arrays.toString(odds));
     }
 
 
     public static void main(String[] args){
-
+        ArrayList pitches = new ArrayList();
         Sample2 sample = new Sample2();
         StringBuffer sb = new StringBuffer();
         sb.append("hello");
         sb.append(" ");
         sb.append("jump to java");
-
+        pitches.add("123");
+        pitches.add("456");
         String result = sb.toString();
         System.out.println(result);
-
+        System.out.println(pitches.get(1));
         sample.stringMethod();
     }
 }
